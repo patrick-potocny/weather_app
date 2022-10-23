20310aee29d55499826691a03be7782ad51d9f52
patrick-potocny <pato.potocny85@gmail.com>
2022-10-23 12:24:56 +0200

    Init commit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+    },
+    "ignorePatterns": ['webpack.config.js', '.eslintrc.js', 'dist/bundle.js']
+}
